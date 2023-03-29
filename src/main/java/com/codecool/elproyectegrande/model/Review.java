66f0ecc83a9d7ea2852b29036f0c70303f866feb
Angelica Popescu <106372644+AngelicaPopescu@users.noreply.github.com>
2023-03-29 15:18:30 +0300

    add review for property functional

--- src/main/java/com/codecool/elproyectegrande/model/Review.java
+++ src/main/java/com/codecool/elproyectegrande/model/Review.java
@@ -1,5 +1,8 @@
 package com.codecool.elproyectegrande.model;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class Review {
     private String description;
     private int satisfaction;
