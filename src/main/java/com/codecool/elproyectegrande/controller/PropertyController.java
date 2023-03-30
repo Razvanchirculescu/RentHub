9e0950ecc8b034c91bf513e1651578a9d49085cd
Angelica Popescu <106372644+AngelicaPopescu@users.noreply.github.com>
2023-03-30 10:13:54 +0300

    Controller modified

--- src/main/java/com/codecool/elproyectegrande/controller/PropertyController.java
+++ src/main/java/com/codecool/elproyectegrande/controller/PropertyController.java
@@ -5,11 +5,19 @@ import com.codecool.elproyectegrande.model.Property;
 import com.codecool.elproyectegrande.model.Review;
 import com.codecool.elproyectegrande.service.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@org.springframework.stereotype.Controller
+@RestController
 public class PropertyController {
 
     private PropertyService propertyService;
@@ -20,31 +28,26 @@ public class PropertyController {
     }
 
     @GetMapping("/properties")
-    @ResponseBody
     public List<Property> getAllProperties() {
         return propertyService.getAllProperties();
     }
 
-    @GetMapping("/property/{name}")
-    @ResponseBody
+    @PatchMapping("/properties/{name}")
     public Property getPropertyDetails(@PathVariable String name) {
         return propertyService.getPropertyByName(name);
     }
 
-    @PostMapping("/property/add")
-    @ResponseBody
+    @PostMapping("/properties")
     public void addProperty(@RequestBody Property property) {
         propertyService.addProperty(property);
     }
 
-    @PostMapping("/property/{name}/reviews/add")
-    @ResponseBody
+    @PostMapping("/properties/{name}/reviews")
     public void addPropertyReview(@PathVariable String name, @RequestBody Review review) {
         propertyService.addReviewForProperty(name, review);
     }
 
     @GetMapping("/properties/category")
-    @ResponseBody
     public List<Property> getPropertiesByCategory(@RequestParam String cat) {
         return propertyService.getPropertiesByCategory(CategoryEx.valueOf(cat));
     }
