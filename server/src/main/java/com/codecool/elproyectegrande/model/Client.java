fc8bb0ce36e99c2b9e532e6a9130c80f3e13e77e
Angelica Popescu <106372644+AngelicaPopescu@users.noreply.github.com>
2023-05-02 14:34:37 +0300

    Final version sprint 3

--- server/src/main/java/com/codecool/elproyectegrande/model/Client.java
+++ server/src/main/java/com/codecool/elproyectegrande/model/Client.java
@@ -8,7 +8,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
