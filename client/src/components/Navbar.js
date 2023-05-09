fa4e1562e6e9befdffc7e386d697199eb3eb8c96
razvanchirculescu <razvanchirculescu@gmail.com>
2023-05-09 20:43:47 +0300

    top right login button on landing page changed to user icon, routed to /api/clients/register

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import logoText from "../images/logos/logoText.png"
 import logoImage from "../images/logos/logoImage.png"
 import TextField from "@mui/material/TextField";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import './Navbar.css';
 import ReactLanguages from "./ReactLanguages";
 
@@ -56,9 +57,9 @@ export default function Navbar() {
                 {/*        className="material-icons">login</i></a>*/}
                 {/*</li>*/}
                 <li className="border-t md:border-none">
-                    <a href="http://localhost:3000/api/clients/register-form"
+                    <a href="http://localhost:3000/api/clients/register"
                        className="block md:inline-block px-4 py-3 no-underline text-grey-darkest hover:text-grey-darker"><i
-                        className="material-icons">login</i></a>
+                        className="material-icons"><AccountCircleIcon/></i></a>
                 </li>
 
             </ul>
