57bcb8d6bf4385ff94c0f2274d5dca0faa244a08
razvanchirculescu <razvanchirculescu@gmail.com>
2023-05-10 10:54:31 +0300

    merged Adi, merged Angi, editClientInfo created , work in progress

---
+++ client/src/pages/EditClientInfoPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import EditClientInfo from "../components/EditClientInfo"
+
+export default function EditClientInfoPage() {
+
+    return (
+        <div className="client">
+            <EditClientInfo/>
+        </div>
+    )
+}
