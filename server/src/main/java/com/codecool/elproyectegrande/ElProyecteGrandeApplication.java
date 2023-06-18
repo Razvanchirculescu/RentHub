9eff8bab17cd5a49de76d50c6cb4f206b85c9301
razvanchirculescu <razvanchirculescu@gmail.com>
2023-06-18 12:46:41 +0300

    reservation form error resolved

--- server/src/main/java/com/codecool/elproyectegrande/ElProyecteGrandeApplication.java
+++ server/src/main/java/com/codecool/elproyectegrande/ElProyecteGrandeApplication.java
@@ -1,17 +1,13 @@
 package com.codecool.elproyectegrande;
 
 import com.codecool.elproyectegrande.service.InitService;
-import com.codecool.elproyectegrande.service.PasswordEncoder;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
@@ -28,26 +24,9 @@ public class ElProyecteGrandeApplication {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(@NonNull CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig")
-						.allowedOrigins("http://localhost:3000")
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
-	}
-
 	@PostConstruct
 	public void seedDatabase() {
 		initService.seedDatabase();
 	}
-
-
-
 
 }
