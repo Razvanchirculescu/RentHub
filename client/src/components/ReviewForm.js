1c922e759d8146661279f9af29740bd5c9314bf5
razvanchirculescu <razvanchirculescu@gmail.com>
2023-06-25 11:43:05 +0300

    comments removed, review included in axios response

--- client/src/components/ReviewForm.js
+++ client/src/components/ReviewForm.js
@@ -5,21 +5,31 @@ import Rating from 'react-rating-stars-component';
 
 
 const ReviewForm = ({ propertyId }) => {
-  const { id } = useParams();
 
   const [description, setDescription] = useState('');
   const [satisfaction, setSatisfaction] = useState(0);
   const [showForm, setShowForm] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const clientId = useParams();
+
+
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`http://localhost:8080/properties/${propertyId}/reviews`, {
-        propertyId,
-        description,
-        satisfaction,
-      });
+      const response = await axios.post(`http://localhost:8080/api/clients/${clientId.id}/reviews`, {
+            propertyId,
+            review: {
+              description,
+              satisfaction
+            }
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          });
       setErrorMessage('');
       setShowForm(false);
       console.log(response.data);
@@ -38,36 +48,36 @@ const ReviewForm = ({ propertyId }) => {
   };
 
   return (
-    <div>
-      {showForm ? (
-        <form onSubmit={handleSubmit}>
-          <div className='review'>
-            <label>Description:</label>
-            <input type='text' value={description} onChange={(e) => setDescription(e.target.value)} />
-          </div>
-          <div className='review'>
-            <label>Satisfaction:</label>
-            <Rating
-              count={5}
-              size={24}
-              activeColor='#ffd700'
-              emptyIcon={<i className='far fa-star'></i>}
-              fullIcon={<i className='fas fa-star'></i>}
-              onChange={handleRatingChange}
-              value={satisfaction}
-            />
-          </div>
-          <button className='review-button' type='submit'>
-            Save
-          </button>
-          <div className='error-reservation'>{errorMessage && <p>{errorMessage}</p>}</div>
-        </form>
-      ) : (
-        <button className='review-button' onClick={handleButtonClick}>
-          Add review
-        </button>
-      )}
-    </div>
+      <div>
+        {showForm ? (
+            <form onSubmit={handleSubmit}>
+              <div className='review'>
+                <label>Description:</label>
+                <input type='text' value={description} onChange={(e) => setDescription(e.target.value)} />
+              </div>
+              <div className='review'>
+                <label>Satisfaction:</label>
+                <Rating
+                    count={5}
+                    size={24}
+                    activeColor='#ffd700'
+                    emptyIcon={<i className='far fa-star'></i>}
+                    fullIcon={<i className='fas fa-star'></i>}
+                    onChange={handleRatingChange}
+                    value={satisfaction}
+                />
+              </div>
+              <button className='review-button' type='submit'>
+                Save
+              </button>
+              <div className='error-reservation'>{errorMessage && <p>{errorMessage}</p>}</div>
+            </form>
+        ) : (
+            <button className='review-button' onClick={handleButtonClick}>
+              Add review
+            </button>
+        )}
+      </div>
   );
 };
 
--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,3 +1,1 @@
-// export const API_BASE_URL =
-
 export const ACCESS_TOKEN = "accessToken"
