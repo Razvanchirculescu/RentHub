f63a9e2cf496d12ed28a3b7547351bddd978d8f5
razvanchirculescu <razvanchirculescu@gmail.com>
2023-07-11 00:02:01 +0300

    searchbar searching through countries cities and property names

--- client/src/components/navbar/SearchBar.js
+++ client/src/components/navbar/SearchBar.js
@@ -7,17 +7,17 @@ import './SearchBar.css'
 export const SearchBar = ({ setSearchResults }) => {
     const [input, setInput] = useState("");
 
-
     const fetchSearchData = (value) => {
         fetch(`http://localhost:8080/properties`)
             .then((response) => response.json())
             .then((json) => {
-                const searchResults = json.filter((properties) => {
+                const searchResults = json.filter((property) => {
                     return (
-                        value &&
-                        properties &&
-                        properties.location.country &&
-                        properties.location.country.toLowerCase().includes(value))
+                        (value && property && property.location.city && property.location.city.toLowerCase().includes(value)) ||
+                        (value && property && property.location.country && property.location.country.toLowerCase().includes(value)) ||
+                        (value && property && property.name && property.name.toLowerCase().includes(value))
+                    );
+
                 });
                 console.log(searchResults);
                 setSearchResults(searchResults);
