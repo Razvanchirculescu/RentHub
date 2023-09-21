4587eb1b24313f8884b3e5a9874e4fee37f6ef2e
razvanchirculescu <razvanchirculescu@gmail.com>
2023-09-21 08:56:30 +0300

    dark theme transition removed, code cleanup

--- client/src/components/theme/Toggler.js
+++ client/src/components/theme/Toggler.js
@@ -4,16 +4,6 @@ import styled from "styled-components"
 
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
-// const Button = styled.button`
-//   //background: ${({ theme }) => theme.background};
-//   //border: 2px solid ${({ theme }) => theme.toggleBorder};
-//   color: ${({ theme }) => theme.text};
-//   //border-radius: 20px;
-//   cursor: pointer;
-//   font-size:1rem;
-//   //padding: 0.6rem;
-//   }
-// `
 const Toggle = ({theme,  toggleTheme }) => {
     return (
         <DarkModeIcon onClick={toggleTheme}/>
