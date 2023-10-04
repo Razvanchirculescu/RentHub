f4f3c54623e9fb08408ead4d6938fb232f67dabd
razvanchirculescu <razvanchirculescu@gmail.com>
2023-10-04 16:06:49 +0300

    class component converted to a functional component , added useEffect

--- client/src/components/theme/DarkModeToggle.js
+++ client/src/components/theme/DarkModeToggle.js
@@ -1,60 +1,29 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 
 import "./dark-mode.scss";
 import "./light-mode.scss";
 
-class DarkModeToggle extends Component {
+const DarkModeToggle = () => {
+    const [darkMode, setDarkMode] = useState(() => JSON.parse(localStorage.getItem("DARK_MODE")) === true);
 
-    updateTheme(darkMode) {
+    useEffect(() => {
         const themeLink = document.getElementById("theme-link");
-        if (darkMode) {
-            themeLink.setAttribute("href", "../src/components/theme/dark-mode.scss");
-        } else {
-            themeLink.setAttribute("href", "../src/components/theme/light-mode.scss");
-        }
-    }
-    constructor(props) {
-        super(props);
-        const darkMode = JSON.parse(localStorage.getItem("DARK_MODE")) === true;
-        this.state = {
-            darkMode: darkMode,
-        };
-        this.updateTheme(darkMode);
-        this.handleModeChange = this.handleModeChange.bind(this);
+        themeLink.setAttribute("href", darkMode ? "../src/components/theme/dark-mode.scss" : "../src/components/theme/light-mode.scss");
+        document.body.classList.toggle("dark-mode", darkMode);
+        localStorage.setItem("DARK_MODE", darkMode);
+    }, [darkMode]);
 
-        if (JSON.parse(localStorage.getItem("DARK_MODE")) === true) {
-            document.body.classList.add("dark-mode");
-        }
-    }
-
-    handleModeChange() {
-        if (!this.state.darkMode) {
-            document.body.classList.add("dark-mode");
-        } else {
-            document.body.classList.remove("dark-mode");
-        }
-
-        this.setState({
-            darkMode: !this.state.darkMode,
+    const handleModeChange = () => {
+        setDarkMode(prevDarkMode => {
+            const newDarkMode = !prevDarkMode;
+            return newDarkMode;
         });
-        localStorage.setItem("DARK_MODE", !this.state.darkMode);
-        const newDarkMode = !this.state.darkMode;
+    };
 
-        // Update the theme
-        this.updateTheme(newDarkMode);
-
-        // Update the state and localStorage
-        this.setState({
-            darkMode: newDarkMode,
-        });
-        localStorage.setItem("DARK_MODE", newDarkMode);
-
-    }
-
-    render() {
-        return <DarkModeIcon onClick={this.handleModeChange}>Change Mode</DarkModeIcon>;
-    }
-}
+    return (
+        <DarkModeIcon onClick={handleModeChange}/>
+    );
+};
 
 export default DarkModeToggle;
